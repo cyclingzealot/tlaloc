0649ff1b5c0ac4d541225e3b7c54e50700f7383d
Julien Lamarche <julien.lamarche@gmail.com>
2016-11-06 11:05:24 -0500

    fixed untilHour

--- tlaloc.rb
+++ tlaloc.rb
@@ -95,7 +95,7 @@ forecasts.each { |f|
 
 i=0
 if twitter
-    announceStr = "Your #ottbike #ottweather " + announceStr
+    announceStr = "Your #ottbike #ottweather until #{untilHour}:59: " + announceStr
 
 	if (announceStr + popStr).length > 140
 	    announceStr="Current/Worst: Wc: #{current.windChill}/#{minWindChill}, P: #{current.pcpType}/#{maxPop}; S: #{sunset}\n"
